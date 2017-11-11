57cc5f2dcafe941ac514417e020e8dea768953b5
Yen Tran <tran.yentran@gmail.com>
2017-11-11 15:18:26 -0800

    String Interpolation

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -6,4 +6,9 @@ import { Component } from '@angular/core';
 })
 
 export class ServerComponent {
+  serverId = 10;
+  serverStatus = 'offline';
+  getServerStatus() {
+    return this.serverStatus;
+  }
 }
